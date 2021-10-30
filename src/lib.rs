2854f8f267d543f6a7b7f720e822f856986f9064
Michael Connor Buchan <mikey@blindcomputing.org>
2021-10-30 15:02:28 +0100

    Cleanup: Silence all current clippy warnings
    I don't like doing this, but this is generated code, it will never be
    perfect.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 macro_rules! include_codegen {
     ($($file: ident),*) => {
         $(
-            #[allow(unused_imports, unused_variables)]
+            #[allow(unused_imports, unused_variables, clippy::type_complexity, clippy::bind_instead_of_map, clippy::needless_borrow, clippy::too_many_arguments)]
             pub mod $file {
             include!(concat!(env!("OUT_DIR"), "/", stringify!($file), ".rs"));
             }
